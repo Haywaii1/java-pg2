d75b93d9e4b49a119908860ad2ef0a33b6ad6f9f
haywaii1 <ayotundeonasanya@yahoo.com>
2022-05-20 19:24:41 +0100

    second assign

---
+++ assign 2.js
@@ -0,0 +1,23 @@
+let musician = 3;
+
+    if (musician <= 0) {
+        console.log("not a group");
+    }
+    else if (musician === 1) {
+        console.log("solo");
+    }
+    else if (musician === 2) {
+        console.log("duet");
+    }
+    else if (musician === 3) {
+        console.log("trio");
+    }
+    else if (musician === 4) {
+        console.log("quartet");
+    }
+    else if (musician > 4) {
+        console.log("this is a large group");
+    }
+    else {
+        console,log("new musicians");
+    }
